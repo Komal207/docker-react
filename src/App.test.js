a2b11a540e7e15291ca214fa36f9db6c1e6b7fbb
Komal207 <komalranpise6@gmail.com>
2021-07-26 14:25:06 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from './App';
+//import { render, screen } from '@testing-library/react';
 
 //test('renders learn react link', () => {
   //render(<App />);
   //const linkElement = screen.getByText(/learn react/i);
   //expect(linkElement).toBeInTheDocument();
 //});
-
+import ReactDOM from 'react-dom';
+import App from "./App";
 it('renders without crashing', () => {
   const div=document.createElement('div');
   ReactDOM.render(<App />, div);
